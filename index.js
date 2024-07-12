f6ae9e4166f773f367777af42436b093afd108b8
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-07-13 00:02:20 +0700

    setup .env

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const route = require("./routes/client/index.route");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
