b092431fa8a4f3333e90c8deeb913c1146b131df
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-07-21 01:00:31 +0700

    setup layout admin

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
 
+const systemConfig = require("./config/system");
+
 require("dotenv").config();
 
 const app = express();
@@ -15,6 +17,8 @@ const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.use(express.static("public"));
 
