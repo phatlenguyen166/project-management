e94c82ce7988fc3d11f4155efc4148822f277a75
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-07-22 00:51:53 +0700

    search optimization part 2

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
 module.exports.index = async (req, res) => {
     const filterStatus = filterStatusHelper(req.query);
 
@@ -10,13 +11,10 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     }
 
-    let keyword = "";
+    const objecSearch = searchHelper(req.query);
 
-    if (req.query.keyword) {
-        keyword = req.query.keyword;
-
-        const regex = new RegExp(keyword,"i");
-        find.title = regex; 
+    if (objecSearch.regex) {
+        find.title = objecSearch.regex;
     }
     const products = await Product.find(find);
 
@@ -24,7 +22,7 @@ module.exports.index = async (req, res) => {
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword
+        keyword: objecSearch.keyword
     });
 
 }
