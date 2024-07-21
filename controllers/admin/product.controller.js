6798e0fae6065f17479f06ffb13e8baea1651670
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-07-22 00:02:08 +0700

    Filter Status Product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,13 +1,43 @@
 const Product = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
+    let filterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ]
+    if (req.query.status) {
+        const index = filterStatus.findIndex(item => item.status == req.query.status);
+        filterStatus[index].class = "active";
+    } else {
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus[index].class = "active";
+    }
+
+    let find = {
         deleted: false
-    });
-    console.log(products);
+    }
+    if (req.query.status) {
+        find.status = req.query.status;
+    }
+    const products = await Product.find(find);
+
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
-        products: products
-        
+        products: products,
+        filterStatus: filterStatus
     });
+
 }
