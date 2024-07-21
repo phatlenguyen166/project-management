5de1ddd47b9eb0eadbedc882b22b8eb893fb575d
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-07-22 00:25:30 +0700

    search product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -32,12 +32,22 @@ module.exports.index = async (req, res) => {
     if (req.query.status) {
         find.status = req.query.status;
     }
+
+    let keyword = "";
+
+    if (req.query.keyword) {
+        keyword = req.query.keyword;
+
+        const regex = new RegExp(keyword,"i");
+        find.title = regex; 
+    }
     const products = await Product.find(find);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: keyword
     });
 
 }
