b400cb22bbb2b99d92e166878097c6992ba6f73c
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-07-24 03:15:44 +0700

    Optimize pagination

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 module.exports.index = async (req, res) => {
     const filterStatus = filterStatusHelper(req.query);
 
@@ -19,23 +20,15 @@ module.exports.index = async (req, res) => {
     }
 
     // Pagination 
-    let objectPagination = {
+    const countProducts = await Product.countDocuments(find);
+
+    let objectPagination = paginationHelper({
         currentPage: 1,
         limitItem: 4
-    };
-
-    if (req.query.page) {
-        objectPagination.currentPage = parseInt(req.query.page);
-    } else {
-
-    }
-    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
-
-    const countProducts = await Product.countDocuments(find);
-    const totalPage = Math.ceil(countProducts / objectPagination.limitItem);
-    objectPagination.totalPage = totalPage;
-    console.log(totalPage);
-
+    },
+        req.query,
+        countProducts
+    )
     // End Pagination 
 
 
