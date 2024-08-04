0d214defb2051941ea29d59f4249cfbaa5f5496b
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-08-05 02:40:29 +0700

    upadte

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -139,6 +139,8 @@ module.exports.changeStatus = async (req, res) => {
 module.exports.changeMulti = async (req, res) => {
     const type = req.body.type;
     const ids = req.body.ids.split(", ");
+    console.log(ids);
+    
     switch (type) {
         case "active":
             await Product.updateMany({ _id: { $in: ids } }, { status: "active" });
