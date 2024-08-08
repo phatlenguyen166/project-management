bd1e9a7ec73d15af66c3a2e62295b6ddf908b4cf
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-08-09 01:53:55 +0700

    Edit Product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -202,7 +202,7 @@ module.exports.deleteItem = async (req, res) => {
 }
 
 // [GET] /admin/products/create
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
     res.render("admin/pages/products/create", {
         pageTitle: "Danh sách sản phẩm",
     });
@@ -264,4 +264,22 @@ module.exports.editPatch = async (req, res) => {
     }
 
     res.redirect(`back`);
+}
+// [GET] /admin/products/detail/:id
+module.exports.detail = async (req, res) => {
+    try {
+        let find = {
+            _id: req.params.id,
+            deleted: false
+        }
+
+        const product = await Product.findOne(find);
+
+        res.render("admin/pages/products/detail", {
+            pageTitle: "Chi tiết sản phẩm",
+            product: product
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/products`);
+    }
 }
